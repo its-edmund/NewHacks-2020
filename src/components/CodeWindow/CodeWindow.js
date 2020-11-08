618d3469a96265c94f16f2b34bc8a77444b1509a
Enzo Cabaltica <enzocollegemail@gmail.com>
2020-11-07 18:51:39 -0800

    Old Stuff Rip Google

--- src/components/CodeWindow/CodeWindow.js
+++ src/components/CodeWindow/CodeWindow.js
@@ -4,8 +4,8 @@ import { Message } from 'semantic-ui-react'
 const CodeWindow = () => {
     return(
         <div>
-            <Message>
-                public static void main(String args[]){'{'} <br/>
+            <Message style={{height: 400}}>
+                public s    tatic void main(String args[]){'{'} <br/>
                 &emsp; System.out.println(); <br/>
                 {'{'} <br/>
             </Message>
